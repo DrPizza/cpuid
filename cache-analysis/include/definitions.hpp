33dd433e50787cbdb7b62b4097753269c6b0f216
Peter Bright <drpizza@quiscalusmexicanus.org>
2017-08-18 17:23:38 -0400

    Rename project

--- cache-analysis/include/definitions.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-constexpr size_t iteration_count = 100'000ULL;
-constexpr uint64_t nanoseconds_per_second = 1'000'000'000ui64;
--- cache-analysis/include/measurement.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-extern uint64_t tick_rate;
-extern uint64_t measurement_overhead;
-
-uint64_t get_actual_frequency();
-uint64_t get_measurement_overhead();
