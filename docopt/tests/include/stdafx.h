e8bcc725f33c06b74d2030e235f15b94978d06ed
Peter Bright <drpizza@quiscalusmexicanus.org>
2019-02-27 22:05:22 -0500

    Start migrating tests to google test.

---
+++ docopt/tests/include/stdafx.h
@@ -0,0 +1,169 @@
+#if !defined(PCH__H)
+#define PCH__H
+
+#if defined(_WIN32)
+
+#include <SDKDDKVer.h>
+
+#if !defined(_STL_EXTRA_DISABLED_WARNINGS)
+#define _STL_EXTRA_DISABLED_WARNINGS 4061 4324 4365 4514 4571 4582 4583 4623 4625 4626 4710 4774 4820 4987 5026 5027 5039
+#endif
+
+#if !defined(_SCL_SECURE_NO_WARNINGS)
+#define _SCL_SECURE_NO_WARNINGS 1
+#endif
+
+#if !defined(_CRT_SECURE_NO_WARNINGS)
+#define _CRT_SECURE_NO_WARNINGS 1
+#endif
+
+#if !defined(_SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING)
+#define _SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING 1
+#endif
+
+#if !defined(_SILENCE_CXX17_OLD_ALLOCATOR_MEMBERS_DEPRECATION_WARNING)
+#define _SILENCE_CXX17_OLD_ALLOCATOR_MEMBERS_DEPRECATION_WARNING 1
+#endif
+
+#if !defined(_SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING)
+#define _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING 1
+#endif
+
+#define STRICT
+#define NOMINMAX
+
+#pragma warning(disable: 4668) // warning C4668: '%s' is not defined as a preprocessor macro, replacing with '0' for '#if/#elif'
+#pragma warning(disable: 4710) // warning C4710: '%s': function not inlined
+#pragma warning(disable: 4711) // warning C4711: function '%s' selected for automatic inline expansion
+#pragma warning(disable: 4820) // warning C4820: '%s': '%d' bytes padding added after data member '%s'
+#pragma warning(disable: 5045) // warning C5045: Compiler will insert Spectre mitigation for memory load if /Qspectre switch specified
+
+#include <Windows.h>
+
+#pragma warning(push)
+#pragma warning(disable: 4005) // warning C4005: '%s': macro redefinition
+#include <Winternl.h>
+#include <ntstatus.h>
+#pragma warning(pop)
+
+#else // defined(_WIN32)
+
+// linux system headers go here
+
+#endif
+
+#if defined(_MSC_VER)
+
+// currently broken (triggers on iterators and other objects that are usually unnamed)
+#pragma warning(disable: 26444) // warning C26444: Avoid unnamed objects with custom construction and destruction (es.84: http://go.microsoft.com/fwlink/?linkid=862923).
+
+// disable for everything
+#pragma warning(disable: 4061) // warning C4061: enumerator '%s' in switch of enum '%s' is not explicitly handled by a case label
+#pragma warning(disable: 4324) // warning C4234: structure was padded due to alignment specifier
+#pragma warning(disable: 4514) // warning C4514: '%s': unreferenced inline function has been removed
+#pragma warning(disable: 4623) // warning C4623: '%s': default constructor was implicitly defined as deleted
+#pragma warning(disable: 4625) // warning C4625: '%s': copy constructor was implicitly defined as deleted
+#pragma warning(disable: 4626) // warning C4626: '%s': assignment operator was implicitly defined as deleted
+#pragma warning(disable: 4710) // warning C4710: '%s': function not inlined
+#pragma warning(disable: 4820) // warning C4820: '%s': '%d' bytes padding added after data member '%s'
+#pragma warning(disable: 5026) // warning C5026: '%s': move constructor was implicitly defined as deleted
+#pragma warning(disable: 5027) // warning C5027: '%s': move assignment operator was implicitly defined as deleted
+
+#pragma warning(disable: 26412) // warning C26412: Do not dereference an invalid pointer (lifetimes rule 1). 'return of %s' was invalidated at line %d by 'no initialization'.
+#pragma warning(disable: 26426) // warning C26426: Global initializer calls a non-constexpr function '%s' (i.22: http://go.microsoft.com/fwlink/?linkid=853919).
+#pragma warning(disable: 26481) // warning C26481: Don't use pointer arithmetic. Use span instead. (bounds.1: http://go.microsoft.com/fwlink/p/?LinkID=620413)
+#pragma warning(disable: 26482) // warning C26482: Only index into arrays using constant expressions (bounds.2: http://go.microsoft.com/fwlink/p/?LinkID=620414).
+#pragma warning(disable: 26485) // warning C26485: Expression '%s::`vbtable'': No array to pointer decay. (bounds.3: http://go.microsoft.com/fwlink/p/?LinkID=620415)
+#pragma warning(disable: 26490) // warning C26490: Don't use reinterpret_cast. (type.1: http://go.microsoft.com/fwlink/p/?LinkID=620417)
+#pragma warning(disable: 26499) // warning C26499: Could not find any lifetime tracking information for '%s'
+
+// disable for standard headers
+#pragma warning(push)
+#pragma warning(disable: 26400) // warning C26400: Do not assign the result of an allocation or a function call with an owner<T> return value to a raw pointer, use owner<T> instead. (i.11 http://go.microsoft.com/fwlink/?linkid=845474)
+#pragma warning(disable: 26401) // warning C26401: Do not delete a raw pointer that is not an owner<T>. (i.11: http://go.microsoft.com/fwlink/?linkid=845474)
+#pragma warning(disable: 26408) // warning C26408: Avoid malloc() and free(), prefer the nothrow version of new with delete. (r.10 http://go.microsoft.com/fwlink/?linkid=845483)
+#pragma warning(disable: 26409) // warning C26409: Avoid calling new and delete explicitly, use std::make_unique<T> instead. (r.11 http://go.microsoft.com/fwlink/?linkid=845485)
+#pragma warning(disable: 26411) // warning C26411: The parameter '%s' is a reference to unique pointer and it is never reassigned or reset, use T* or T& instead. (r.33 http://go.microsoft.com/fwlink/?linkid=845479)
+#pragma warning(disable: 26412) // warning C26412: Do not dereference an invalid pointer (lifetimes rule 1). 'return of %s' was invalidated at line %d by 'end of function scope (local lifetimes end)'.
+#pragma warning(disable: 26413) // warning C26413: Do not dereference nullptr (lifetimes rule 2). 'nullptr' was pointed to nullptr at line %d.
+#pragma warning(disable: 26423) // warning C26423: The allocation was not directly assigned to an owner.
+#pragma warning(disable: 26424) // warning C26424: Failing to delete or assign ownership of allocation at line %d.
+#pragma warning(disable: 26425) // warning C26425: Assigning '%s' to a static variable.
+#pragma warning(disable: 26444) // warning C26444: Avoid unnamed objects with custom construction and destruction (es.84: http://go.microsoft.com/fwlink/?linkid=862923).
+#pragma warning(disable: 26461) // warning C26461: The reference argument '%s' for function %s can be marked as const. (con.3: https://go.microsoft.com/fwlink/p/?LinkID=786684)
+#pragma warning(disable: 26471) // warning C26471: Don't use reinterpret_cast. A cast from void* can use static_cast. (type.1: http://go.microsoft.com/fwlink/p/?LinkID=620417).
+#pragma warning(disable: 26481) // warning C26481: Don't use pointer arithmetic. Use span instead. (bounds.1: http://go.microsoft.com/fwlink/p/?LinkID=620413)
+#pragma warning(disable: 26482) // warning C26482: Only index into arrays using constant expressions. (bounds.2: http://go.microsoft.com/fwlink/p/?LinkID=620414)
+#pragma warning(disable: 26490) // warning C26490: Don't use reinterpret_cast. (type.1: http://go.microsoft.com/fwlink/p/?LinkID=620417)
+#pragma warning(disable: 26493) // warning C26493: Don't use C-style casts that would perform a static_cast downcast, const_cast, or reinterpret_cast. (type.4: http://go.microsoft.com/fwlink/p/?LinkID=620420)
+#pragma warning(disable: 26494) // warning C26494: Variable '%s' is uninitialized. Always initialize an object. (type.5: http://go.microsoft.com/fwlink/p/?LinkID=620421)
+#pragma warning(disable: 26495) // warning C26495: Variable '%s' is uninitialized. Always initialize a member variable. (type.6: http://go.microsoft.com/fwlink/p/?LinkID=620422)
+#pragma warning(disable: 26496) // warning C26496: Variable '%s' is assigned only once, mark it as const. (con.4: https://go.microsoft.com/fwlink/p/?LinkID=784969)
+#pragma warning(disable: 26497) // warning C26497: This function %s could be marked constexpr if compile-time evaluation is desired. (f.4: https://go.microsoft.com/fwlink/p/?LinkID=784970)
+
+#endif
+
+#include <cstddef>
+#include <map>
+#include <set>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <regex>
+#include <iomanip>
+#include <type_traits>
+#include <utility>
+#include <memory>
+#include <tuple>
+#include <thread>
+#include <cstdlib>
+
+#if defined(_MSC_VER)
+
+// disable additionally for third-party libraries
+#pragma warning(push)
+#pragma warning(disable: 4571) // warning: C4571: Informational: catch(...) semantics changed since Visual C++ 7.1; structured exceptions (SEH) are no longer caught
+
+#pragma warning(disable: 26429) // warning C26429: Symbol '%s' is never tested for nullness, it can be marked as not_null (f.23: http://go.microsoft.com/fwlink/?linkid=853921).
+#pragma warning(disable: 26432) // warning C26432: If you define or delete any default operation in the type '%s', define or delete them all (c.21: http://go.microsoft.com/fwlink/?linkid=853922).
+#pragma warning(disable: 26433) // warning C26433: Function '%s' should be marked with 'override' (c.128: http://go.microsoft.com/fwlink/?linkid=853923).
+#pragma warning(disable: 26434) // warning C26434: Function '%s' hides a non-virtual function '%s' (c.128: http://go.microsoft.com/fwlink/?linkid=853923).
+#pragma warning(disable: 26436) // warning C26436: The type '%s' with a virtual function needs either public, virtual or protected non-virtual destructor (c.35: http://go.microsoft.com/fwlink/?linkid=853924).
+#pragma warning(disable: 26439) // warning C26439: This kind of function may not throw. Declare it 'noexcept' (f.6: http://go.microsoft.com/fwlink/?linkid=853927).
+#pragma warning(disable: 26440) // warning C26440: Function '%s' can be declared 'noexcept' (f.6: http://go.microsoft.com/fwlink/?linkid=853927).
+#pragma warning(disable: 26443) // warning C26443: Overriding destructor should not use explicit 'override' or 'virtual' specifiers (c.128: http://go.microsoft.com/fwlink/?linkid=853923).
+#pragma warning(disable: 26462) // warning C26462: The value pointed to by '%s' is assigned only once, mark it as a pointer to const (con.4: https://go.microsoft.com/fwlink/p/?LinkID=784969).
+#pragma warning(disable: 26472) // warning C26472: Don't use a static_cast for arithmetic conversions. Use brace initialization, gsl::narrow_cast or gsl::narow (type.1: http://go.microsoft.com/fwlink/p/?LinkID=620417).
+#pragma warning(disable: 26474) // warning C26474: Don't cast between pointer types when the conversion could be implicit (type.1: http://go.microsoft.com/fwlink/p/?LinkID=620417).
+#pragma warning(disable: 26491) // warning C26491: Don't use static_cast downcasts (type.2: http://go.microsoft.com/fwlink/p/?LinkID=620418).
+#pragma warning(disable: 26498) // warning C26498: The function '%s' is constexpr, mark variable '%s' constexpr if compile-time evaluation is desired (con.5: https://go.microsoft.com/fwlink/p/?LinkID=784974).
+
+#endif
+
+#ifndef BOOST_CONFIG_SUPPRESS_OUTDATED_MESSAGE
+#define BOOST_CONFIG_SUPPRESS_OUTDATED_MESSAGE
+#endif
+
+#include <boost/algorithm/string.hpp>
+#include <boost/regex.hpp>
+
+#include <gsl/gsl>
+
+#include <fmt/format.h>
+
+#include <gtest/gtest.h>
+
+#if defined(_MSC_VER)
+
+#pragma warning(pop)
+#pragma warning(pop)
+
+#endif
+
+#define DOCOPT_USE_BOOST_REGEX 1
+
+#endif
+
+
---
+++ docopt/tests/src/test.cpp
@@ -0,0 +1,140 @@
+#include "stdafx.h"
+
+#include "docopt/docopt.hpp"
+
+using ::testing::TestWithParam;
+using ::testing::ValuesIn;
+
+namespace
+{
+	struct docopt_test_data
+	{
+		std::string usage;
+		std::vector<std::string> argv;
+		std::string expected_result;
+		std::size_t test_number;
+		std::size_t subtest_number;
+	};
+
+	const auto results_to_json = [&] (const std::map<std::string, docopt::value>& results) {
+		bool first = true;
+		std::string json;
+		json += "{";
+		for(auto const& arg : results) {
+			if(first) {
+				first = false;
+			} else {
+				json += ", ";
+			}
+			json += '"';
+			json += arg.first;
+			json += "\": ";
+			json += std::visit(docopt::value_printer, arg.second);
+		}
+		json += "}";
+		return json;
+	};
+
+	std::vector<docopt_test_data> load_data() {
+		wchar_t dir[260];
+		::GetModuleFileNameW(nullptr, dir, 260);
+		wchar_t* i = dir + std::wcslen(dir);
+		while(*--i != L'\\') {
+			*i = '\0';
+		}
+		::SetCurrentDirectoryW(dir);
+
+		std::ifstream fin("../../../docopt/tests/data/docopt.testcases");
+		std::stringstream buffer;
+		buffer << fin.rdbuf();
+		const std::string whole_file = buffer.str();
+
+		const std::regex comment_pattern(" *#.*?\n");
+		const std::string no_comments = std::regex_replace(whole_file, comment_pattern, "");
+
+		const std::string section_start = "r\"\"\"";
+		auto start = no_comments.find(section_start);
+		auto end = no_comments.find(section_start, start + section_start.size());
+
+		std::vector<std::string> raw_sections;
+		for(; end != std::string::npos; start = end, end = no_comments.find(section_start, start + section_start.size())) {
+			raw_sections.push_back(std::string(no_comments.data() + start + section_start.size(), no_comments.data() + end));
+		}
+		raw_sections.push_back(no_comments.substr(start + section_start.size()));
+
+		std::vector<docopt_test_data> command_lines;
+		const std::string section_end = "\"\"\"";
+		const std::string invocation_start = "$ prog";
+		const std::regex  newline("\n");
+		std::size_t test_number = 0;
+		for(std::string const& section : raw_sections) {
+			const std::string usage_section = section.substr(0, section.find(section_end));
+			const std::string all_invocations = section.substr(section.find(section_end) + section_end.size());
+
+			std::vector<std::string> raw_invocations;
+			auto s = all_invocations.find(invocation_start);
+			auto e = all_invocations.find(invocation_start, s + invocation_start.size());
+			for(; e != std::string::npos; s = e, e = all_invocations.find(invocation_start, s + invocation_start.size())) {
+				raw_invocations.push_back(std::string(all_invocations.data() + s + invocation_start.size(), all_invocations.data() + e));
+			}
+			raw_invocations.push_back(all_invocations.substr(s + invocation_start.size()));
+			std::size_t subtest_number = 0;
+			for(std::string const& inv : raw_invocations) {
+				const std::string args = inv.substr(0, inv.find("\n"));
+				std::string expected = std::regex_replace(inv.substr(inv.find("\n") + 1), newline, "");
+
+				if(expected[0] == '"') {
+					expected = expected.substr(1);
+				}
+				if(expected[expected.size() - 1] == '"') {
+					expected = expected.substr(0, expected.size() - 1);
+				}
+
+				docopt_test_data data = {
+					usage_section, docopt::crack_argv(args), expected, test_number, subtest_number
+				};
+				command_lines.push_back(data);
+				++subtest_number;
+			}
+			++test_number;
+		}
+		return command_lines;
+	}
+}
+
+const std::vector<docopt_test_data> command_lines = load_data();
+
+struct DocoptTest : TestWithParam<docopt_test_data>
+{
+};
+
+TEST_P(DocoptTest, ParserTest) {
+	docopt_test_data data = GetParam();
+	try {
+		auto result = docopt::docopt_parse(data.usage, data.argv, false, false);
+		const std::string json = results_to_json(result);
+		EXPECT_EQ(data.expected_result, json);
+	} catch(std::exception& e) {
+		EXPECT_EQ(data.expected_result, e.what());
+	}
+}
+
+std::string param_printer(testing::TestParamInfo<docopt_test_data> data) {
+	std::string index_padding = data.index < 10  ? "00"
+	                          : data.index < 100 ? "0"
+	                          :                    "";
+
+	std::string test_padding = data.param.test_number < 10  ? "00"
+	                         : data.param.test_number < 100 ? "0"
+	                         :                                "";
+
+	std::string subtest_padding = data.param.subtest_number < 10  ? "00"
+	                            : data.param.subtest_number < 100 ? "0"
+	                            :                                   "";
+
+	return index_padding + std::to_string(data.index)
+	     + "Section" + test_padding + std::to_string(data.param.test_number)
+	     + "Params" + subtest_padding + std::to_string(data.param.subtest_number);
+}
+
+INSTANTIATE_TEST_CASE_P(DocoptFullTests, DocoptTest, ValuesIn(command_lines), param_printer);
